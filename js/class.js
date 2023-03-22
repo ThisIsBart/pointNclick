e60714fd0da318087cd15d71be0280da6f11aa7d
BartVanHier <47629122+BartVanHier@users.noreply.github.com>
2023-03-22 12:54:47 +0100

    added interactables and audio

--- js/class.js
+++ js/class.js
@@ -5,7 +5,7 @@ const offsetCharacterY = 16;
 
 const mainCharacter = document.getElementById("mainCharacter");
 const gameWindow = document.getElementById("gameWindow");
-
+const heroSounds = document.getElementById("heroSounds");
 
 gameWindow.onclick = function(event){
     var rect = gameWindow.getBoundingClientRect();
@@ -14,4 +14,18 @@ gameWindow.onclick = function(event){
     mainCharacter.style.left = x - offsetCharacterX + 'px';
     mainCharacter.style.top = y - offsetCharacterY + 'px';
     //console.log(x);
+    switch(event.target.id) {
+        case "door1":
+            //do something
+            console.log("knock knock");
+            heroSounds.play();
+            break;
+        case "platform":
+            //do something else
+            console.log("nice and dry");
+            break;
+        default:
+            console.log("hmm...");
+            break;
+    }
 }
