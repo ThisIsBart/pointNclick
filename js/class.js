150e57e46d0dafe22550a7d5b348d795231ba2ee
BartVanHier <47629122+BartVanHier@users.noreply.github.com>
2023-03-24 11:38:32 +0100

    updated tilemap and conversation bar

--- js/class.js
+++ js/class.js
@@ -6,6 +6,7 @@ const offsetCharacterY = 16;
 const mainCharacter = document.getElementById("mainCharacter");
 const gameWindow = document.getElementById("gameWindow");
 const heroSounds = document.getElementById("heroSounds");
+const mainCharacterSpeech = document.getElementById("mainCharacterSpeech");
 
 gameWindow.onclick = function(event){
     var rect = gameWindow.getBoundingClientRect();
@@ -14,18 +15,31 @@ gameWindow.onclick = function(event){
     mainCharacter.style.left = x - offsetCharacterX + 'px';
     mainCharacter.style.top = y - offsetCharacterY + 'px';
     //console.log(x);
+
     switch(event.target.id) {
         case "door1":
             //do something
-            console.log("knock knock");
+            showSpeechBubble("Lovely day for it!");
             heroSounds.play();
             break;
         case "platform":
             //do something else
-            console.log("nice and dry");
+            showSpeechBubble("What's all this, then? <br> Something new?");
+            break;
+        case "ghostpic":
+            showSpeechBubble("Top 'o the mornin'!<br>You know of a good haunt near here?")
             break;
         default:
-            console.log("hmm...");
+            hideSpeechBubble();
             break;
     }
+}
+
+function showSpeechBubble(dialogue) {
+    mainCharacterSpeech.innerHTML = dialogue;
+    mainCharacterSpeech.style.opacity = 1;
+}
+function hideSpeechBubble() {
+    mainCharacterSpeech.style.opacity = 0;
+    mainCharacterSpeech.innerHTML = " ... ";
 }
