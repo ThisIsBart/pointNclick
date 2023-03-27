4dc63f8fc17b2d1d38f0c6085d7ea6b097bd4c67
BartVanHier <47629122+BartVanHier@users.noreply.github.com>
2023-03-28 00:21:55 +0200

    conversation

--- js/class.js
+++ js/class.js
@@ -7,39 +7,46 @@ const mainCharacter = document.getElementById("mainCharacter");
 const gameWindow = document.getElementById("gameWindow");
 const heroSounds = document.getElementById("heroSounds");
 const mainCharacterSpeech = document.getElementById("mainCharacterSpeech");
+const otherSpeech = document.getElementById("otherSpeech");
 
 gameWindow.onclick = function(event){
+    
     var rect = gameWindow.getBoundingClientRect();
     var x = event.clientX - rect.left; // event's x position
     var y = event.clientY - rect.top; // event's y position
     mainCharacter.style.left = x - offsetCharacterX + 'px';
     mainCharacter.style.top = y - offsetCharacterY + 'px';
     //console.log(x);
-
+    document.getElementById("platformPic").style.opacity = 1;
     switch(event.target.id) {
         case "door1":
             //do something
-            showSpeechBubble("Lovely day for it!");
+            showSpeechBubble("Top 'o the mornin'!", mainCharacterSpeech);
             heroSounds.play();
             break;
         case "platform":
             //do something else
-            showSpeechBubble("What's all this, then? <br> Something new?");
+            event.target.style.opacity = 0;
+            document.getElementById("platformPic").style.opacity = 0.5;
+            showSpeechBubble("What's all this, then? <br> Something new?", mainCharacterSpeech);
             break;
         case "ghostpic":
-            showSpeechBubble("Top 'o the mornin'!<br>You know of a good haunt near here?")
+            showSpeechBubble("Lovely day for it!<br>You know of a good haunt near here?", mainCharacterSpeech);
+            setTimeout(function(){showSpeechBubble("Ya'll should totally check out that there wooden platform.<br>I'm, like, pretty sure it hides a secret.", otherSpeech)}, 5000);
+            setTimeout(function(){showSpeechBubble("I'll go an' 'ave me a gander then.<br>Cheers!", mainCharacterSpeech)}, 10000);
             break;
         default:
-            hideSpeechBubble();
+            hideSpeechBubble(mainCharacterSpeech);
             break;
     }
 }
 
-function showSpeechBubble(dialogue) {
-    mainCharacterSpeech.innerHTML = dialogue;
-    mainCharacterSpeech.style.opacity = 1;
+function showSpeechBubble(dialogue, bubble) {
+    bubble.innerHTML = dialogue;
+    bubble.style.opacity = 1;
+    setTimeout(function(){hideSpeechBubble(bubble)}, 5000);
 }
-function hideSpeechBubble() {
-    mainCharacterSpeech.style.opacity = 0;
-    mainCharacterSpeech.innerHTML = " ... ";
+function hideSpeechBubble(bubble) {
+    bubble.style.opacity = 0;
+    bubble.innerHTML = " ... ";
 }
